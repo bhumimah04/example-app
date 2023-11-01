0a887da236a422fe3fccb3030f91bdf68ab7d982
bhumimah04 <bhumimah04@gmail.com>
2023-11-01 10:19:25 +0530

    Number Swapping Program

---
+++ practicals/numberSwap.php
@@ -0,0 +1,16 @@
+<?php
+// Define two numbers
+$number1 = 5;
+$number2 = 10;
+
+echo "Before swapping: ";
+echo "Number 1 = " . $number1 . ", Number 2 = " . $number2 . "<br>";
+
+// Swap the numbers using a temporary variable
+$temp = $number1;
+$number1 = $number2;
+$number2 = $temp;
+
+echo "After swapping: ";
+echo "Number 1 = " . $number1 . ", Number 2 = " . $number2 . "<br>";
+?>
